a9ee794b3a018ff1d8f7ee0328e6d691318a121f
ahmet sarikaya <ahmetsarikaya0696@outlook.com>
2023-07-21 09:43:58 +0300

    working with multiple slices

--- src/store/index.js
+++ src/store/index.js
@@ -18,10 +18,27 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      // login logic...
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export default store;
